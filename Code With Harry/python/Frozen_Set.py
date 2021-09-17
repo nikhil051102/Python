03b459f3af8f5f9fca41b526a343d1b05e8af3aa
NikhilDeore <64294981+nikhil051102@users.noreply.github.com>
2021-09-17 16:41:31 +0530

    Delete Code With Harry/python directory

--- Code With Harry/python/Frozen_Set.py
+++
@@ -1,8 +0,0 @@
-#FrozenSet is a new class that has the characteristics of set but its elements cannot be changed once assigned.
-#Like Tuple are immutable lists, Frozensets are immutable sets.
-
-
-set1 = frozenset({1, 2, 3, 4, 5})
-set2 = frozenset({4, 5, 6, 7, 8})
-print(set1)
-print(set1.intersection(set2)) #We can do intersection, union, symmetric_difference, difference operation in Frozenset but we cant add, remove, etc operation in frozenset because it is immutable.
--- Code With Harry/python/Notes.py
+++
@@ -1,3 +0,0 @@
-# 1) Difference between set.remove() and set.discard()
-# we want to remove 'x' from set1 in which 'x' is not present, if we perform this action by set1.discard(x) then it will not throw error and program will continue but while performing s1.remove(x) then program will throw error and terminate program because x is not present
-
